150b340ef2a27281b014a36182e47505c7c2dedb
Francesco Fioravanti <francesco.cantante@hotmail.it>
2020-10-21 19:30:30 +0200

    productkey api

---
+++ TTControlPanel/Controllers/Api/ProductKeyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TTControlPanel.Services;
+
+namespace TTControlPanel.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/ProductKey")]
+    [ApiController]
+    public class ProductKeyController : ControllerBase
+    {
+        private readonly DBContext _dB;
+
+        public ProductKeyController(DBContext dB)
+        {
+            _dB = dB ?? throw new ArgumentNullException(nameof(dB));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string productKey)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(productKey))
+                    return NotFound(new { });
+                var lic = await _dB.Licenses
+                    .Include(l => l.ProductKey)
+                    .Where(l => l.ProductKey.Key == productKey)
+                    .FirstOrDefaultAsync();
+                if (lic == null)
+                    return NotFound(new { });
+                return Ok(new { });
+            }
+            catch { return NotFound(new { }); }
+        }
+    }
+}
