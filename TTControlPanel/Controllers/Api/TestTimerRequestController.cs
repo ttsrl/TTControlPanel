1b0c8d3786838f4aee554c7396d8254c8457427c
Francesco Fioravanti <francesco.cantante@hotmail.it>
2020-10-29 23:05:27 +0100

    orders controller

---
+++ TTControlPanel/Controllers/Api/TestTimerRequestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TTControlPanel.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/TestTimerRequest")]
+    [ApiController]
+    public class TestTimerRequestController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int returnValue)
+        {
+            await Task.Delay(10000);
+            if (returnValue == 200)
+                return Ok();
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ TTControlPanel/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TTControlPanel.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
